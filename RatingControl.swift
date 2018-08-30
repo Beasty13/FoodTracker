767c69dd064e387f670f9e9aa2727fca02a02622
Tyler Nelson <beasty@tylers-macbook-pro.local>
2018-08-30 10:04:26 -0600

    I committed!

---
+++ RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Tyler Nelson on 8/30/18.
+//  Copyright © 2018 Tyler Nelson. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
